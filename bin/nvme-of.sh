fd6b27ef8235d63319b0ca71d619e10123993a0c
MXHML <mxhml@proton.me>
2026-09-03 00:57:18 -0400

    add basic NVMe-oF helper script

---
+++ bin/nvme-of.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+(( $UID != 0 )) && exit 1
+read -rp "Enter NQN: " nqn
+set -euo pipefail
+modprobe nvme nvme-tcp nvme-fabrics
+
+tee /etc/modules-load.d/nvme-of.conf > /dev/null << 'EOF'
+nvme
+nvme-core
+nvme-tcp
+nvme-fabrics
+EOF
+
+tee /etc/initramfs-tools/hooks/nvme-of > /dev/null << EOF
+nvme connect -t tcp -n ${nqn} -a 172.16.63.16 -s 4420
+EOF
+chmod +x /etc/initramfs-tools/hooks/nvme-of
+update-initramfs -u -k all
